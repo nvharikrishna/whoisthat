7e0fb4c8a3a907d8fbbe2aeecf6027c7c4e7a261
N V Harikrishna <Harikrishna@Venkatas-MacBook-Pro.local>
2016-09-11 11:46:22 +0530

    Removed un-necessary buttons from UI.

--- app/src/main/java/com/nvharikrishna/whoisthat/MainActivity.java
+++ app/src/main/java/com/nvharikrishna/whoisthat/MainActivity.java
@@ -50,23 +50,23 @@ public class MainActivity extends AppCompatActivity {
         startService(new Intent(this, NotificationListenerService.class));
         setContentView(R.layout.activity_main);
 
-        toggle = (ToggleButton)findViewById(R.id.speechToggle);
-        smsText = (TextView)findViewById(R.id.sms_text);
-        smsSender = (TextView)findViewById(R.id.sms_sender);
+       // toggle = (ToggleButton)findViewById(R.id.speechToggle);
+        // smsText = (TextView)findViewById(R.id.sms_text);
+        // smsSender = (TextView)findViewById(R.id.sms_sender);
 
-        toggleListener = new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
-                if(isChecked){
-                    speaker.allow(true);
-                    speaker.speak(getString(R.string.start_speaking));
-                }else{
-                    speaker.speak(getString(R.string.stop_speaking));
-                    speaker.allow(false);
-                }
-            }
-        };
-        toggle.setOnCheckedChangeListener(toggleListener);
+//        toggleListener = new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+//                if(isChecked){
+//                    speaker.allow(true);
+//                    speaker.speak(getString(R.string.start_speaking));
+//                }else{
+//                    speaker.speak(getString(R.string.stop_speaking));
+//                    speaker.allow(false);
+//                }
+//            }
+//        };
+//        toggle.setOnCheckedChangeListener(toggleListener);
 
 //        checkTTS();
 //        initializeSMSReceiver();
